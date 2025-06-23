cfa420ab3fb9c47d09e4cc2e5f22ceffd4af5af2
ajallen14 <ajallen142001@gmail.com>
2025-06-23 23:34:33 +0530

    Weather Forecast Cards

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -17,7 +17,6 @@ class WeatherScreen extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {},
-            splashColor: Color.fromRGBO(71, 137, 224, 1),
             focusColor: Color.fromRGBO(71, 137, 224, 1),
             icon: Icon(Icons.refresh),
           ),
@@ -32,7 +31,8 @@ class WeatherScreen extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: Card(
-                elevation: 20,
+                elevation: 15,
+                shadowColor: Color.fromRGBO(71, 137, 224, 1),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(23),
                 ),
@@ -68,40 +68,56 @@ class WeatherScreen extends StatelessWidget {
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
             ),
             SizedBox(height: 10),
-            Row(
-              children: [
-                SizedBox(
-                  width: 100,
-                  child: Card(
-                    child: Column(
-                      children: [
-                        Text(
-                          "9:00",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(width: 15),
-                        Icon(Icons.cloud, size: 30),
-                        SizedBox(width: 15),
-                        Text("Rain", style: TextStyle(fontSize: 17)),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Placeholder(
-              // weatherforecast cards
-              fallbackHeight: 160,
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  SizedBox(width: 100, child: HourlyForecastCards()),
+                  SizedBox(width: 100, child: HourlyForecastCards()),
+                  SizedBox(width: 100, child: HourlyForecastCards()),
+                  SizedBox(width: 100, child: HourlyForecastCards()),
+                  SizedBox(width: 100, child: HourlyForecastCards()),
+                ],
+              ),
             ),
             const SizedBox(height: 20),
-            const Placeholder(
-              // weatherforecast cards
-              fallbackHeight: 150,
+            Text(
+              "Additional Information",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class HourlyForecastCards extends StatelessWidget {
+  const HourlyForecastCards({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 20,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: SizedBox(
+            width: 100,
+            child: Column(
+              children: [
+                Text(
+                  "9:00",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(width: 17),
+                Icon(Icons.cloud, size: 30),
+                SizedBox(width: 17),
+                Text("30°K", style: TextStyle(fontSize: 17)),
+              ],
+            ),
+          ),
         ),
       ),
     );
